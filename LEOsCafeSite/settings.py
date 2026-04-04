3922e2a2b9b3c5a5852cb79b027c738391fb1dad
陳霖 <chenlin@chenlindeMacBook-Pro.local>
2026-04-04 17:17:40 +0800

    Add STATIC_ROOT for deployment

--- LEOsCafeSite/settings.py
+++ LEOsCafeSite/settings.py
@@ -127,9 +127,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# --- 新增以下這行 ---
+# 這告訴 Django 執行 collectstatic 時，要把檔案存在伺服器上的哪個資料夾
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
